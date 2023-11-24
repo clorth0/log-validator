e4b54bfb81b204e4dd8876219b7a1559fd1d3dc8
Matt James <mattj@odnd.com>
2023-11-24 11:47:22 -0600

    Syslog conformity

--- log-validator.py
+++ log-validator.py
@@ -1,46 +1,43 @@
 import re
+from datetime import datetime
 
-# Regular expressions for different formats
-timestamp_regex = r'your-timestamp-format-regex'  # Replace with your actual timestamp regex
-mac_address_regex = r'([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})'
-ipv4_regex = r'(\d{1,3}\.){3}\d{1,3}'
-ipv6_regex = r'([0-9a-fA-F]{0,4}:){7}[0-9a-fA-F]{0,4}'
-session_id_regex = r'your-session-id-regex'  # Replace with your actual session ID regex
-asn_regex = r'your-asn-regex'  # Replace with your actual ASN regex
-event_id_regex = r'your-event-id-regex'  # Replace with your actual event ID regex
-status_code_regex = r'\d{3}'  # Assuming a 3-digit status code
-response_time_regex = r'your-response-time-regex'  # Replace with your actual response time regex
-username_regex = r'your-username-regex'  # Replace if needed
-command_regex = r'your-command-regex'  # Replace if needed
+# Regular expressions for syslog format
+timestamp_regex = r'\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2}'  # Example: 'Jan  1 00:00:00'
 
-# Function to validate timestamp
 def is_valid_timestamp(timestamp):
-    return bool(re.match(timestamp_regex, timestamp))
+    try:
+        # Adjust the format based on your syslog timestamp format
+        datetime.strptime(timestamp, '%b %d %H:%M:%S')
+        return True
+    except ValueError:
+        return False
 
-# Function to validate MAC address
-def is_valid_mac(mac):
-    return bool(re.match(mac_address_regex, mac))
+def parse_syslog_entry(entry):
+    # Example syslog format: <Priority>Timestamp Hostname Tag: Message
+    # Adjust parsing logic as per your syslog format
+    parts = entry.split(' ', 4)
+    if len(parts) < 5:
+        return None, 'Incomplete entry'
 
-# Function to validate IP addresses
-def is_valid_ip(ip):
-    return bool(re.match(ipv4_regex, ip) or re.match(ipv6_regex, ip))
+    priority, timestamp, hostname, tag, message = parts
+    return {
+        'priority': priority,
+        'timestamp': timestamp,
+        'hostname': hostname,
+        'tag': tag.rstrip(':'),
+        'message': message
+    }, None
 
-# ... similar functions for other patterns
+def validate_syslog_entry(entry):
+    fields, error = parse_syslog_entry(entry)
+    if error:
+        return False, error
 
-def validate_log_entry(entry):
-    # Assuming the log entry is in key-value pair format like "key=value"
-    fields = dict(field.split('=') for field in entry.split(' ') if '=' in field)
-    
     errors = []
-
-    # Validate each field
-    if not is_valid_timestamp(fields.get('timestamp', '')):
+    if not is_valid_timestamp(fields['timestamp']):
         errors.append('Invalid timestamp')
 
-    if not is_valid_mac(fields.get('device_id', '')):
-        errors.append('Invalid MAC address')
-
-    # Add similar checks for other fields...
+    # Add more validations as per your requirements
 
     return len(errors) == 0, ', '.join(errors)
 
@@ -48,10 +45,10 @@ def main(log_file_path):
     with open(log_file_path, 'r') as file:
         for line in file:
             entry = line.strip()
-            is_valid, error = validate_log_entry(entry)
+            is_valid, error = validate_syslog_entry(entry)
             if not is_valid:
                 print(f"Invalid entry: {entry}\nError: {error}")
 
 if __name__ == "__main__":
-    log_file_path = 'path_to_your_log_file.log'
+    log_file_path = 'path_to_your_syslog_file.log'
     main(log_file_path)
